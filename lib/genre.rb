c5fca2dec5b37814e11954b807c3d809e086beeb
louslife6788 <34876425+louslife6788@users.noreply.github.com>
2021-07-05 12:17:24 -0400

    Update genre.rb

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,28 @@
+  
 class Genre
-  attr_accessor :name
 
-  @@all = []
+    attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+    @@all = []
+     
+    def initialize(type)
+        @name = type
+        @@all << self 
+    end
 
-  def self.all
-    @@all
-  end
 
-  def songs
-    Song.all { |song| song.genre == self }
-  end
+    def self.all
+        @@all
+    end
 
-  def artists
-    songs.map(&:artist)
-  end
+    def songs
+        Song.all { |song| song.genre == self }
+    end
 
-  def add_song(song)
-    songs << song
-  end
+    def artists
+        songs.map{|song| song.artist}.uniq
+    end
+    
+    
+
 end
