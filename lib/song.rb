b2fccb573e394d81515d4244a10066ce2f0ad512
luis leon <leonluis380@yahoo.com>
2021-07-05 10:40:21 -0400

    commit tree 4d3ca74f721b211b0b445aefa1761e47701aa32f

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,24 @@
+class Song 
+    @@all = []
 
+    attr_accessor  :genre, :artist
+    attr_reader :name
+    def initialize(name, artist, genre)
+        @name = name 
+        @artist = artist
+        @genre = genre 
+        @@all << self 
+    end 
+
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+
+end 
